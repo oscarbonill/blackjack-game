fcbff6f67271347d1ac1b3a323b280ec559b2e56
Oscar <oscarbonill@gmail.com>
2018-08-14 22:31:05 -0400

    commit changes to script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-console.log("hello");
+//Create blackjack game
+//TODO: Create Deck
+    //Create array of Suits
+    //Create array of Values
+    //Create empty array
+    var Suits = ["Diamonds","Clubs","Hearts","Spades"];
+    var Values = ["Ace","King","Queen","Jack","Ten","Nine","Eight","Seven","Six",
+                "Five","Four","Three","Two"];
+    var Deck = [ ];
+    //Create function that will create deck
+    function CreateDeck()
+    {
+        for(var i = 0; i < Suits.length;i++)
+        {
+            for(var j = 0; j < Values.length; j++)
+            {
+                var card = {
+                    Suit : Suits[i],
+                    Value : Values[j]
+                }
+                Deck.push(card);
+            }
+        }
+    }
 
-console.log("I have added additional info to commit and push");
+    CreateDeck();
+
+    //Show card function
+    function ShowCard(card)
+    {
+        console.log(card.Value);
+    }
+
+    ShowCard(Deck[1]);
+    ShowCard(Deck[2]);
+   
+    
+//TODO: Deal player cards
+//TODO: Deal House Cards
